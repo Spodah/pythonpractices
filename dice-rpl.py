7d5472cd37f49683dcd21212e2e9fc33c9bd86cb
Juuso Puroila <jpuroila@gmail.com>
2017-06-06 18:45:31 +0300

    Add statistics to diceroller

--- dice-rpl.py
+++ dice-rpl.py
@@ -15,9 +15,34 @@ def test(dice_string, amount):
 		dice_tuples.append(tuple(temp))
 	dice_total = []
 	for i in range(0, amount):
+		die = 0
 		for y in dice_tuples:
-			dice_total.append(roll(y))
+			die = die +(roll(y))
+		dice_total.append(die)
 	return dice_total
 
-die = test("1d6+1d8", 20)
+def get_stats(results):
+	maximum_result = max(results)
+	dicetionary = {}
+	dicetionary2 = {}
+	for i in range(1, maximum_result+1):
+		temp = results.count(i)
+		if (temp>0):
+			dicetionary.update({i:temp})
+	for x in range(1, maximum_result+1):
+		z = 0
+		temp = 0
+		for y in results:
+			if(y==x):
+				z=z+1
+			else:
+				if(z>temp):
+					temp = z
+					z = 0
+		dicetionary2.update({x:temp})
+	average = sum(results)/len(results)
+	statistics = {'count' : dicetionary, 'sequence' : dicetionary2, 'average' : average}
+	return statistics
+
+die = get_stats(test("1d6", 2000))
 print(die)
